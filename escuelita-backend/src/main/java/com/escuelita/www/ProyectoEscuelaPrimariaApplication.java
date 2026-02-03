c53840c1173d9a0910b26e2b256111981f5c56b4
Oicapse <c.berrulo@unsm.edu.pe>
2026-02-03 13:30:04 -0500

    joy gei

---
+++ escuelita-backend/src/main/java/com/escuelita/www/ProyectoEscuelaPrimariaApplication.java
@@ -0,0 +1,13 @@
+package com.escuelita.www;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoEscuelaPrimariaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectoEscuelaPrimariaApplication.class, args);
+	}
+
+}
---
+++ escuelita-backend/src/test/java/com/escuelita/www/ProyectoEscuelaPrimariaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.escuelita.www;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectoEscuelaPrimariaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

721417ab016b4ba4c5de7aa308fa4fe45e1db637
Oicapse <c.berrulo@unsm.edu.pe>
2026-02-23 02:19:10 -0500

    Mala creacion de carpetas

--- src/main/java/com/escuelita/www/repository/PromediosPeriodoRepository.java
+++
@@ -1,9 +0,0 @@
-package com.escuelita.www.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-import com.escuelita.www.entity.PromediosPeriodo;
-
-@Repository
-public interface PromediosPeriodoRepository extends JpaRepository<PromediosPeriodo, Long> {
-}
--- src/main/java/com/escuelita/www/repository/TiposNotaRepository.java
+++
@@ -1,9 +0,0 @@
-package com.escuelita.www.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-import com.escuelita.www.entity.TiposNota;
-
-@Repository
-public interface TiposNotaRepository extends JpaRepository<TiposNota, Long> {
-}
